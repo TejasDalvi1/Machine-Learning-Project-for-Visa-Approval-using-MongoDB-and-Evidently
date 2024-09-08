4e8c544db3a0e601f1b48e79c2797fde0ee03d69
Tejas Dalvi <tejasdalvi.coep@gmail.com>
2024-09-08 20:22:06 +0530

    Established connection with MongoDB.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="visa_approval",
+    version="0.0.1",
+    author="Tejas Dalvi",
+    author_email="tejasdalvi.coep@gmail.com",
+    packages=find_packages()
+)
